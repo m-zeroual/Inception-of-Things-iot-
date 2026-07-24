a7eda222307a813e22e0c6f4f63ed45609cb9b1c
m-zeroual <mohammed.zeroual.mz@gmail.com>
2026-07-24 11:32:00 +0200

    change K3S_TOKEN with my username.

--- p1/scripts/provisionS.sh
+++ p1/scripts/provisionS.sh
@@ -3,4 +3,4 @@
 apt-get update -y
 apt-get install curl bash -y
 
-curl -sfL https://get.k3s.io | K3S_TOKEN="dokoko_cluster_token" INSTALL_K3S_EXEC="server --node-ip=192.168.56.110 --write-kubeconfig-mode=644" sh -s
+curl -sfL https://get.k3s.io | K3S_TOKEN="mzeroual_cluster_token" INSTALL_K3S_EXEC="server --node-ip=192.168.56.110 --write-kubeconfig-mode=644" sh -s
